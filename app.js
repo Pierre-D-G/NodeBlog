60fd2313bebec47ecac98dc5b3bd37f3ff51063a
Pierre-D-G <gamil.pierre@gmail.com>
2017-05-01 20:06:16 -0400

    Dependancies and App Middleware setup

---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    ExpressValidator = require('express-validator'),
+    mongo = require('mongodb'),
+    db = require('monk')('localhost/nodeblog'),
+    multer = require('multer'),
+    flash = require('connect-flash')
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// File uploads and multipart data handling
+app.use(multer({dest: './public/iamges/uploads'}).single('photo'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+// bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Express Validator middleware
+app.use(ExpressValidator({
+ errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle express sessions
+
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Flash Messages
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Make the database accessable to the router
+app.use(function(req, res, next){
+  req.db = db;
+  next()
+});
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
