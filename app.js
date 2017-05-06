fd77b05a80c32e6c371143ca1f7042ba7b0d6102
Pierre-D-G <gamil.pierre@gmail.com>
2017-05-06 19:10:38 -0400

    Express handlebars helpers and more Design Tweaks

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express');
-    path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    bodyParser = require('body-parser'),
-    ExpressValidator = require('express-validator'),
-    mongo = require('mongodb'),
-    db = require('monk')('localhost/nodeblog'),
-    multer = require('multer'),
-    path = require('path');
-    flash = require('connect-flash'),
-    Handlebars = require("handlebars");
+path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  ExpressValidator = require('express-validator'),
+  mongo = require('mongodb'),
+  db = require('monk')('localhost/nodeblog'),
+  multer = require('multer'),
+  path = require('path');
+  flash = require('connect-flash'),
+  moment = require('moment'),
+  exphbs = require('express-handlebars');
 
 
 var index = require('./routes/index');
@@ -20,38 +21,58 @@ var categories = require('./routes/categories');
 
 var app = express();
 
-app.locals.shortenBody = function(text, length){
-  var shortBody = text.subString(0, length);
-  return shortBody;
-}
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+var hbs = exphbs.create({
+  extname: ".hbs",
+  layoutsDir: path.join(__dirname, "views/layouts/"),
+  defaultLayout: 'layout.hbs',
+  helpers: {
+    truncate: function (passString) {
+      var theString = passString.substring(0, 400);
+      return theString + '...';
+    },
+    formatDate: function (datetime, format) {
+      var DateFormats = {
+        long: "dddd DD MMMM YYYY"
+      };
+      if (moment) {
+        format = DateFormats[format] || format;
+        return moment(datetime).format(format);
+      } else {
+        return datetime; 
+      }
+    }
+  }
+});
 
-// File uploads and multipart data handling
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 // bodyParser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 // Express Validator middleware
 app.use(ExpressValidator({
- errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+  errorFormatter: function (param, msg, value) {
+    var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
 
-    while(namespace.length) {
+    while (namespace.length) {
       formParam += '[' + namespace.shift() + ']';
     }
     return {
-      param : formParam,
-      msg   : msg,
-      value : value
+      param: formParam,
+      msg: msg,
+      value: value
     };
   }
 }));
@@ -76,7 +97,7 @@ app.use(function (req, res, next) {
 });
 
 // Make the database accessable to the router
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   req.db = db;
   next()
 });
@@ -86,15 +107,18 @@ app.use('/', index);
 app.use('/posts', posts);
 app.use('/categories', categories);
 
+// helpers
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
